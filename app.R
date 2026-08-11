373b6d704536c13e9aff0f0a075f7e49ccf6f2b0
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-08-11 15:23:13 -0700

    update package install info

--- app.R
+++ app.R
@@ -1,3 +1,12 @@
 # Launch the ShinyApp (Do not remove this comment)
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+options(repos = BiocManager::repositories())
+
+if (!requireNamespace("BiocCertificate", quietly = TRUE))
+    BiocManager::install("Bioconductor/BiocCertificate")
+
 BiocCertificate::BiocCertificate()
+
